8017450b67d3a801efac2c326581b1294991fa1a
Nakendraprasath K <nakendraprasath321@gmail.com>
2022-08-10 22:35:53 +0530

    Time: 3 ms (97.52%), Space: 7.9 MB (72.21%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>mem(n+1);
+        mem[0]=0;
+        for(int i=0; i<=n; i++) mem[i] = mem[i/2] + i%2;
+        return mem;
+    }
+};
